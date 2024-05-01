7b514150eb90d5a042ad5f27a8beaacc752da089
titouanthd <t.thebaud@infonet-group.com>
2024-05-01 23:31:04 +0200

    empty keywords fix

--- src/services/RemoveDuplicatedKeywordsService.ts
+++ src/services/RemoveDuplicatedKeywordsService.ts
@@ -8,23 +8,24 @@ export class RemoveDuplicatedKeywordsService {
         }
 
 
-        // we must remove all empty keywords
-        const emptyKeywords = keywords.filter((keyword) => {
-            return keyword.value === '';
+        // we must remove all empty keywords and undefined keywords
+        const notEmptyKeywords = keywords.filter((keyword) => {
+            return keyword.value !== undefined && keyword.value !== '';
         });
 
         // sort keywords alphabetically
-        emptyKeywords.sort((a, b) => {
+        notEmptyKeywords.sort((a, b) => {
+            console.log(a.value, b.value);
             return a.value.localeCompare(b.value);
         });
         // sort keywords by length
-        emptyKeywords.sort((a, b) => {
+        notEmptyKeywords.sort((a, b) => {
             return b.value.length - a.value.length;
         });
 
         // save updated keywords
         let content = 'value\n';
-        emptyKeywords.forEach((keyword) => {
+        notEmptyKeywords.forEach((keyword) => {
             content += keyword.value + '\n';
         });
         // delete old file
